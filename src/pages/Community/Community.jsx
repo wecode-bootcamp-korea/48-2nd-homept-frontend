e919ad3f1ea8f12dafb623c4505eb9d270c316c6
정효은 <junghyoeun@jeonghyoeun-ui-MacBookPro.local>
2023-09-08 17:32:06 +0900

    ADD : Community UI 구현중

--- src/pages/Community/Community.jsx
+++ src/pages/Community/Community.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import './Community.scss';
+import CommunityNav from './components/CommunityNav';
+import CommunityList from './components/CommunityList';
 
 const Community = () => {
-  return <div>Community</div>;
+  return (
+    <div>
+      <div>
+        <CommunityNav />
+        <CommunityList />
+      </div>
+    </div>
+  );
 };
 
 export default Community;
