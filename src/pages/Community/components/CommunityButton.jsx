ffc1c88c7e8ce4084d698a34cba4ffe56bcd648f
정효은 <junghyoeun@jeonghyoeun-ui-MacBookPro.local>
2023-09-09 20:46:06 +0900

    Modify: UI구현, 리스트 필터링 완료

---
+++ src/pages/Community/components/CommunityButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CommunityButton.scss';
+
+const CommunityButton = () => {
+  return (
+    <div className="CommunityButton">
+      <button className="button">글쓰기</button>
+    </div>
+  );
+};
+
+export default CommunityButton;
