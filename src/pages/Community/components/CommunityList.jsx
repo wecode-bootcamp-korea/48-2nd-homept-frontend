5278b4f42401ff0ebd63fdcf8dd6c98d59fca051
정효은 <junghyoeun@jeonghyoeun-ui-MacBookPro.local>
2023-09-09 20:46:06 +0900

    Modify: UI구현, 리스트 필터링 완료

--- src/pages/Community/components/CommunityList.jsx
+++ src/pages/Community/components/CommunityList.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import './CommunityList.scss';
 
-const CommunityList = () => {
+const CommunityList = ({ filteredData }) => {
   return (
     <div className="CommunityList">
-      <div className="buttonWrap">
-        <button className="button">자유</button>
-        <span>🥈감자1</span>
-      </div>
+      {filteredData.map(item => (
+        <div className="container" key={item.id}>
+          <div className="buttonWrap">
+            <button
+              className={`button ${
+                item.buttonStyle === 'coaching' ? 'coaching' : 'free'
+              }`}
+            >
+              {item.category}
+            </button>
+            <span className="userName">{item.userName}</span>
+          </div>
+          <div className="titleContainer">
+            <div className="titleWrap">
+              <div className="title">{item.title}</div>
+              <div className="content">{item.content}</div>
+            </div>
+            <img className="image" src={item.image} alt="이미지" />
+          </div>
+          <div className="comment">
+            <span>{item.time}</span>
+            <span>{item.comments}</span>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
--- src/pages/Community/components/CommunityNav.jsx
+++ src/pages/Community/components/CommunityNav.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import './CommunityNav.scss';
 
 const CommunityNav = () => {
-  return (
-    <>
-      <div className="CommunityNav">커뮤니티</div>
-    </>
-  );
+  return <div className="CommunityNav">커뮤니티</div>;
 };
 
 export default CommunityNav;
