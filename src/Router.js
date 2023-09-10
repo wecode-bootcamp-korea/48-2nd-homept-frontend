02811b079b45d69a58b5573bae00e6ff8cddc86a
wanwork <wjdqo9705@gmail.com>
2023-09-10 19:54:37 +0900

    Add: MyPage Ui implementation complete

--- src/Router.js
+++ src/Router.js
@@ -4,6 +4,7 @@ import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/SignUp/SignUp';
 import Community from './pages/Community/Community';
 import MyPage from './pages/MyPage/MyPage';
+import MyPageEditing from './pages/MyPage/MyPageEditing';
 import Payment from './pages/Payment/Payment';
 import PersonalTraining from './pages/PersonalTraining/PersonalTraining';
 
@@ -15,6 +16,7 @@ const Router = () => {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/community" element={<Community />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/mypage-editing" element={<MyPageEditing />} />
         <Route path="/payment" element={<Payment />} />
         <Route path="/personal-training" element={<PersonalTraining />} />
       </Routes>
--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,7 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from './components/Button/Button';
+import Info from './components/Info/Info';
+import { INPUT_DATA, BUTTON_DATA } from './inputData.js';
+import './MyPage.scss';
 
 const MyPage = () => {
-  return <div>MyPage</div>;
+  const [userInfo, setUserInfo] = useState({});
+  const goToMyPageEditing = useNavigate('/mypage-editing');
+  const physicalInfoData = INPUT_DATA.physicalInfo;
+
+  const getUserInfo = () => {
+    fetch('/data/userData.json', {
+      method: 'GET',
+      headers: {
+        // 'Content-Type': 'application/json;charset=utf-8',
+        // authorization: localStorage.getItem('TOKEN'),
+      },
+    })
+      .then(res => res.json())
+      .then(result => {
+        setUserInfo(result.data);
+      });
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  return (
+    <div className="myPageContainer">
+      <div className="userInfoContainer">
+        <div className="infoNickName">
+          <span>🥇</span>
+          <span>NickName</span>
+        </div>
+
+        <div className="infoList">
+          <Info
+            className={physicalInfoData[0].className}
+            infoName={physicalInfoData[0].infoName}
+            userdata={userInfo.height}
+            measure="cm"
+          />
+          <Info
+            className={physicalInfoData[1].className}
+            infoName={physicalInfoData[1].infoName}
+            userdata={userInfo.weight}
+            measure="kg"
+          />
+        </div>
+      </div>
+      <Button
+        className="btnEditing"
+        text="수정하기"
+        onClickFunction={goToMyPageEditing}
+      />
+    </div>
+  );
 };
 
 export default MyPage;
