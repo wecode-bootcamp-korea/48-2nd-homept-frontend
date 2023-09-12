337a4a196134a90bfb84f3925c2300f508298c0b
sand <138185427+hyoeunjung@users.noreply.github.com>
2023-09-12 17:20:08 +0900

    Revert "ADD : Post UI 구현"
    This reverts commit 348d3902f88d1efdc6e87c0c6d86da7cdef648a9.

--- src/Router.js
+++ src/Router.js
@@ -3,11 +3,9 @@ import Nav from './components/Nav/Nav';
 import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/SignUp/SignUp';
 import Community from './pages/Community/Community';
-import Post from './pages/Community/Post';
 import MyPage from './pages/MyPage/MyPage';
 import Payment from './pages/Payment/Payment';
 import PersonalTraining from './pages/PersonalTraining/PersonalTraining';
-import PostDetail from './pages/Community/PostDetail';
 
 const Router = () => {
   return (
@@ -16,9 +14,6 @@ const Router = () => {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/community" element={<Community />} />
-        <Route path="/community/post" element={<Post />} />
-        <Route path="/community/postdetail" element={<PostDetail />} />
-
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/payment" element={<Payment />} />
         <Route path="/personal-training" element={<PersonalTraining />} />
