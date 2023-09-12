f5822d1d9647535f1be6be6be4eba056e172ac72
sand <138185427+hyoeunjung@users.noreply.github.com>
2023-09-12 17:20:08 +0900

    Revert "Modify: UI구현, 리스트 필터링 완료"
    This reverts commit ffc1c88c7e8ce4084d698a34cba4ffe56bcd648f.

--- src/pages/Community/components/CommunityList.jsx
+++ src/pages/Community/components/CommunityList.jsx
@@ -1,34 +1,13 @@
 import React from 'react';
 import './CommunityList.scss';
 
-const CommunityList = ({ filteredData }) => {
+const CommunityList = () => {
   return (
     <div className="CommunityList">
-      {filteredData.map(item => (
-        <div className="container" key={item.id}>
-          <div className="buttonWrap">
-            <button
-              className={`button ${
-                item.buttonStyle === 'coaching' ? 'coaching' : 'free'
-              }`}
-            >
-              {item.category}
-            </button>
-            <span className="userName">{item.userName}</span>
-          </div>
-          <div className="titleContainer">
-            <div className="titleWrap">
-              <div className="title">{item.title}</div>
-              <div className="content">{item.content}</div>
-            </div>
-            <img className="image" src={item.image} alt="이미지" />
-          </div>
-          <div className="comment">
-            <span>{item.time}</span>
-            <span>{item.comments}</span>
-          </div>
-        </div>
-      ))}
+      <div className="buttonWrap">
+        <button className="button">자유</button>
+        <span>🥈감자1</span>
+      </div>
     </div>
   );
 };
