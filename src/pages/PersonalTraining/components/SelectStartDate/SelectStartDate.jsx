b471528d6920513d2649a03c8219c4b52fcf9d18
Uwnag2 <126768997+jeonmye@users.noreply.github.com>
2024-01-25 17:48:43 +0900

    Fix : 운동 날짜 설정 기능 수정

--- src/pages/PersonalTraining/components/SelectStartDate/SelectStartDate.jsx
+++ src/pages/PersonalTraining/components/SelectStartDate/SelectStartDate.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
 import 'moment/locale/ko';
 import moment from 'moment';
@@ -6,6 +6,8 @@ import postSelectedStartDate from '../../../../API/personalTrainingAPI/postSelec
 import './SelectStartDate.scss';
 
 const SelectStartDate = ({ setPtStartDate, ptStartDate, selectedButton }) => {
+  const [isDateSet, setIsDateSet] = useState(false);
+
   const selectedStartDate =
     ptStartDate && moment(ptStartDate).format('YYYY-MM-DD');
 
@@ -15,6 +17,16 @@ const SelectStartDate = ({ setPtStartDate, ptStartDate, selectedButton }) => {
       {moment(date).format('dddd').slice(0, 1)})
     </>;
   };
+
+  const hanledClick = selectedStartDate => {
+    const fetchData = async () => {
+      await postSelectedStartDate(selectedStartDate);
+      alert('날짜 설정이 완료되었습니다');
+      setIsDateSet(true);
+    };
+    fetchData();
+  };
+
   return (
     <div className="selectStartDate">
       <div className="calendarTitle">운동 시작 날짜를 선택해주세요</div>
@@ -32,7 +44,7 @@ const SelectStartDate = ({ setPtStartDate, ptStartDate, selectedButton }) => {
         <button
           disabled={selectedButton}
           className="seletedDateBox"
-          onClick={() => postSelectedStartDate(selectedStartDate)}
+          onClick={() => hanledClick(selectedStartDate)}
         >
           {formatDate(ptStartDate)}
           선택
