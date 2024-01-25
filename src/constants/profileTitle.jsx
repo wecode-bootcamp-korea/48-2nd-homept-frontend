af2c676b97ee20ec8768d0a472467d16c3f611f9
Uwnag2 <126768997+jeonmye@users.noreply.github.com>
2024-01-25 11:39:24 +0900

    Refactor : 리펙토링

--- src/constants/profileTitle.jsx
+++ src/constants/profileTitle.jsx
@@ -1,5 +1,7 @@
-export const PROFILE_CONTENT_TITLE = [
+const PROFILE_CONTENT_TITLE = [
   { title: 'LICENSE', keyText: 'license' },
   { title: 'CAREER', keyText: 'career' },
   { title: 'AWARDS', keyText: 'awards' },
 ];
+
+export default PROFILE_CONTENT_TITLE;
